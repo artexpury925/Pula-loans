f2c7ad24b4f3bf7cd4165e593f443301a94c7252
artexpury925 <artexpury@gmail.com>
2025-11-28 21:34:25 +0300

    Create frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        botswana: { blue: "#0055A4", light: "#CEDDE8", dark: "#0A1F3D" }
+      }
+    }
+  },
+  plugins: [],
+}
